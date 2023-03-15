1e106cfbdf9bcdc1e694f20a229deacfa40dc240
Nastassja Motro <nastassjamotro17@gmail.com>
2023-03-15 13:07:49 -0600

    Update Commands.sql

--- Final_Project/Commands.sql
+++ Final_Project/Commands.sql
@@ -65,7 +65,85 @@ CREATE OR ALTER PROCEDURE ChangeEmployeeSelection
 )
 AS
 BEGIN
-    DECLARE
+    DECLARE @EmployeeSelectionID INT,
+            @BenefitID SMALLINT;
+    SET @BenefitID = (
+           SELECT B.BenefitsID
+           FROM EmployeeSelection AS ES
+           WHERE ES.BenefitsID = @BenefitID AND @EmployeeID = ES.EmployeeID AND ES.StopDate IS NULL
+    )
+    IF EXISTS -- check for duplicate entry and print out the existing entry
+    (
+           SELECT ES.EmployeeSelectionID
+           FROM EmployeeSelection AS ES
+           WHERE ES.EmployeeSelectionID = @EmployeeSelectionID AND ES.EmployeeID = @EmployeeID AND ES.BenefitsID = @BenefitsID AND ES.PlanID = @NewPlanID AND ES.LevelID = @NewLevelID
+    )
+           BEGIN
+              SELECT * FROM EmployeeSelection AS ES
+              WHERE ES.EmployeeSelectionID = @EmployeeSelectionID AND ES.EmployeeID = @EmployeeID AND ES.BEnefitsID = @BenefitsID AND ES.PlanID = @NewPlanID AND ES.LevelID = @NewLevelID
+           END
+    ELSE
+           IF EXISTS -- Update an existing entry
+           (
+                  SELECT ES.EmployeeSelectionID
+                  FROM EmployeeSelection AS ES
+                  WHERE ES.EmployeeSelectionID = @EmployeeSelectionID AND ES.EmployeeID = @EmployeeID AND ES.StopDate IS NULL AND (ES.BenefitsID != @BeneiftID OR ES.PlanID != @NewPlanID OR ES.LevelID != @NewLevelID)
+           )
+                  BEGIN
+                       BEGIN TRANSACTION
+                       UPDATE EmployeeSelection
+                       SET StopDate = GETDATE()
+                       WHERE EmployeeSelectionID = @EmployeeSelectionID;
+                       
+                       INSERT INTO EmployeeSelection (EmployeeID, BenefitsID, PlanID, LevelID, StartDate)
+                       VALUES (@EmployeeID, @BenefitID, @NewPlanID, @NewLevelID, GETDATE());
+                       COMMIT;
+                  END
+           ELSE
+                  BEGIN -- add a new entry
+                       BEGIN TRANSACTION
+                       INSERT INTO EmployeeSelection (EmployeeID, BenefitsID, PlanID, LevelID, StartDate)
+                       VALUES (@EmployeeID, @BenefitID, @NewPlanID, @NewLevelID, GETDATE());
+                       COMMIT;
+                  END
+END
+
+EXECUTE ChangeEmployeeSelection
+       @EmployeeID = 6,
+       @NewPlanID = 23,
+       @NewLevel = 19,
+       @CurrentBenefit = 'Medical'
+       
+SELECT * FROM EmployeeSelection
+WHERE EmployeeID = 6 
+ORDER BY EmployeeID, BenefitsID
+
+-- Terminating an employee
+
+CREATE OR ALTER PROCEDURE TerminateEmployee
+(
+       @EmployeeID TINYINT,
+       @Reason VARCHAR(100)
+)
+AS
+BEGIN
+       BEGIN TRANSACTION
+       UPDATE Employees
+       SET TerminateDate = GETDATE()
+       WHERE EmployeeID = @EmployeeId
+       UPDATE Employees
+       SET TerminationReason = @Reason
+       WHERE EmployeeID = @EmployeeID
+       UPDATE EmployeeSelection
+       SET StopDate = GETDATE()
+       WHERE EmployeeID = @EmployeeID AND StopDate IS NULL
+       COMMIT;
+END
+
+EXECUTE TerminateEmployee
+       @EmployeeID = 6,
+       @Reason
+
 
 
 
